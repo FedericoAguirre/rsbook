a2b44781de347d7ea5d40b65d35f6f6e9409e705
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-02-20 21:36:31 -0600

    ownership folder renamed

--- ownership_1/src/main.rs
+++ ownership_1/src/main.rs
@@ -2,6 +2,9 @@ fn main() {
     scope();
     mut_string();
     // mem_alloc_error();
+    mem_auto_release();
+    mem_cloning();
+    stack_mem_copy();
 }
 
 fn scope() {
@@ -20,4 +23,25 @@ fn mem_alloc_error() {
     let s2 = s1.clone();
 
     println!("{s1}, world!");
+}
+
+fn mem_auto_release() {
+    let mut s = String::from("hello");
+    s = String::from("ahoy");
+
+    println!("{s}, world!");
+}
+
+fn mem_cloning(){
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+
+    println!("s1 = {s1}, s2 = {s2}");
+}
+
+fn stack_mem_copy(){
+    let x = 5;
+    let y = x;
+
+    println!("x = {x}, y = {y}");
 }
