529a620b63e93b9dd2abe40eccc3871e0e20dc94
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-02-28 23:02:35 -0600

    mut_ref_error implemented

---
+++ ownership_4/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+    //mut_ref_error();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+//fn mut_ref_error() {
+//    let mut s = String::from("hello");
+//    change(&s);
+//}
+
+//fn change(some_string: &String) {
+//    some_string.push_str(", world");
+//}
