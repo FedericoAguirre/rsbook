5f840bd4f9bc4e1f3b74868d536bf58dc731d7ad
Federico Aguirre <federico.aguirre.cardiel@gmail.com>
2025-03-02 21:50:57 -0600

    mtu ref added

--- ownership_4/src/main.rs
+++ ownership_4/src/main.rs
@@ -4,18 +4,41 @@ fn main() {
     let len = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}.");
-    // mut_ref_error();
+    //mut_ref_error();
+    mut_ref();
+    // single_mut_ref_error();
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
 
-// fn mut_ref_error() {
+//fn mut_ref_error() {
 //    let mut s = String::from("hello");
 //    change(&s);
-// }
+//}
 
-// fn change(some_string: &String) {
+//fn change(some_string: &String) {
 //    some_string.push_str(", world");
+//}
+
+fn mut_ref() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+    println!("s: {}", s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// fn single_mut_ref_error() {
+//     let mut s = String::from("hello");
+
+//     let r1 = &mut s;
+//     let r2 = &mut s;
+
+//     println!("{}, {}", r1, r2);
+
 // }
